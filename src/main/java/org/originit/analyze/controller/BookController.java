1506caf491805ed1beb1973057a57e66a4571abe
xxc <17770092259@163.com>
2022-09-12 13:16:07 +0800

    1. 添加更新名称的接口 2. 修复使用thymeleaf没有的th:else bug 3.

--- src/main/java/org/originit/analyze/controller/BookController.java
+++ src/main/java/org/originit/analyze/controller/BookController.java
@@ -9,14 +9,22 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 @Controller
 public class BookController {
@@ -30,6 +38,7 @@ public class BookController {
 
     /**
      * 查询目录，若有分类则查看分类下的目录
+     *
      * @param category 书的分类
      * @throws IOException
      */
@@ -40,17 +49,18 @@ public class BookController {
             category = categoryList.get(0).getName();
         }
         List<Book> books = Collections.EMPTY_LIST;
-        if (category != null){
+        if (category != null) {
             books = handleBooks(category);
         }
-        model.addAttribute("category",category);
-        model.addAttribute("categories",categoryList);
-        model.addAttribute("books",books);
+        model.addAttribute("category", category);
+        model.addAttribute("categories", categoryList);
+        model.addAttribute("books", books);
         return "index";
     }
 
     /**
      * 处理url的中文
+     *
      * @return 编码后的url
      */
     private String encodeArg(String arg) {
@@ -59,6 +69,7 @@ public class BookController {
 
     /**
      * 处理分类下的所有书籍
+     *
      * @param category 分类名称
      * @return 该分类下的书籍的名称分类及详情url
      * @throws IOException
@@ -69,7 +80,7 @@ public class BookController {
         if (booksRes.length > 0) {
             books = new ArrayList<>(booksRes.length);
             for (Resource bookRes : booksRes) {
-                if (!bookRes.isFile()||bookRes.getFile().isFile()) {
+                if (!bookRes.isFile() || bookRes.getFile().isFile()) {
                     continue;
                 }
                 Book book = new Book();
@@ -84,6 +95,7 @@ public class BookController {
 
     /**
      * 处理配置的outside.resource.path下的分类列表
+     *
      * @return 分类的相关信息
      * @throws IOException
      */
@@ -94,7 +106,7 @@ public class BookController {
             categoryList = new ArrayList<>(categoriesRes.length);
             for (Resource resource : categoriesRes) {
                 // 分类应该是文件夹类型
-                if (!resource.isFile()||resource.getFile().isFile()) {
+                if (!resource.isFile() || resource.getFile().isFile()) {
                     continue;
                 }
                 final Category c = new Category();
@@ -109,21 +121,22 @@ public class BookController {
 
     /**
      * 查看书的所有内容
+     *
      * @param category 书分类
      * @param bookName 书名或者所在文件夹
-     * @param link 当前是哪一章
+     * @param link     当前是哪一章
      */
     @RequestMapping("/book")
-    public String index(Model model,@RequestParam String category,@RequestParam String bookName,@RequestParam(required = false) String link) throws IOException {
+    public String index(Model model, @RequestParam String category, @RequestParam String bookName, @RequestParam(required = false) String link) throws IOException {
         Resource[] resources = ResourceUtil.loadResources(path + "/" + category + "/" + bookName + "/*.html");
 
         List<BookDetails> booksDetails = new ArrayList<>(resources.length);
         // 遍历文件内容
-        for(Resource resource : resources) {
+        for (Resource resource : resources) {
             final BookDetails pageInfo = new BookDetails();
             pageInfo.setName(resource.getFilename());
             pageInfo.setUrl(resourcePrefix + "/" + category + "/" + encodeArg(bookName) + "/" + encodeArg(resource.getFilename()));
-            pageInfo.setForwardUrl("/?link="+encodeArg(resource.getFilename()));
+            pageInfo.setForwardUrl("/?link=" + encodeArg(resource.getFilename()));
             booksDetails.add(pageInfo);
         }
         if (link == null) {
@@ -134,11 +147,97 @@ public class BookController {
             // 处理中文
             final String[] split = link.split("/");
             split[split.length - 1] = encodeArg(split[split.length - 1]);
-            link = String.join("",split);
+            link = String.join("", split);
         }
-        model.addAttribute("booksDetails",booksDetails);
-        model.addAttribute("link",link);
-        model.addAttribute("bookName",bookName);
+        model.addAttribute("booksDetails", booksDetails);
+        model.addAttribute("link", link);
+        model.addAttribute("bookName", bookName);
         return "book-details";
+    }
+
+    private void resolveResources(String pattern, Consumer<Resource> resourceConsumer) {
+        resolveResources(pattern, resourceConsumer, null);
+    }
+
+    private void resolveResources(String pattern, Consumer<Resource> resourceConsumer, Predicate<Resource> resourcePredicate) {
+        Assert.notNull(resourceConsumer, "请传入consumer进行处理");
+        Resource[] categoriesRes = ResourceUtil.loadResources(pattern);
+        for (Resource resource : categoriesRes) {
+            if (resourcePredicate != null && !resourcePredicate.test(resource)) {
+                continue;
+            }
+            resourceConsumer.accept(resource);
+        }
+    }
+
+    Predicate<Resource> dirPredicate = resource -> {
+        try {
+            return resource.isFile() && resource.getFile().isDirectory();
+        } catch (IOException e) {
+            return false;
+        }
+    };
+
+    Predicate<Resource> filePredicate = resource -> {
+        try {
+            return resource.isFile() && resource.getFile().isFile();
+        } catch (IOException e) {
+            return false;
+        }
+    };
+
+    @RequestMapping("/formatFiles")
+    @ResponseBody
+    public String formatFiles(@RequestParam(required = false) String category, @RequestParam(required = false) String bookName, @RequestParam String originStr, @RequestParam(defaultValue = "") String replacement, @RequestParam(defaultValue = "true") boolean includeDir) {
+        Function<Resource, File> replaceFunc = res -> {
+            try {
+                final File dest = new File(Objects.requireNonNull(res.getFile().getAbsolutePath()).replace(originStr, replacement));
+                boolean success = res.getFile().renameTo(dest);
+                if (success) {
+                    return dest;
+                } else {
+                    return res.getFile();
+                }
+            } catch (IOException e) {
+                return null;
+            }
+        };
+        if (category == null) {
+            // 修改所有的书
+            resolveResources(path + "/*", resource -> {
+                // 重命名
+                File file = replaceFunc.apply(resource);
+                if (file == null) {
+                    return;
+                }
+                // 递归处理分类下的书籍
+                replaceBooksName(file.getName(), replaceFunc);
+            }, dirPredicate);
+        } else {
+            if (bookName == null) {
+                // 修改具体某本书
+                replaceDetailsName(category, bookName, replaceFunc);
+            } else {
+                // 修改指定分类的书
+                // 递归处理分类下的书籍
+                replaceBooksName(category, replaceFunc);
+            }
+        }
+        ResourceUtil.loadResources(path);
+        return "success";
+    }
+
+    private void replaceBooksName(String category, Function<Resource, File> replaceFunc) {
+        resolveResources(path + "/" + category + "/*", bookRes -> {
+            final File bookFile = replaceFunc.apply(bookRes);
+            if (bookFile == null) {
+                return;
+            }
+            replaceDetailsName(category, bookFile.getName(), replaceFunc);
+        }, dirPredicate);
+    }
+
+    private void replaceDetailsName(String category, String bookName, Function<Resource, File> replaceFunc) {
+        resolveResources(path + "/" + category + "/" + bookName + "/*.html", replaceFunc::apply, filePredicate);
     }
 }
